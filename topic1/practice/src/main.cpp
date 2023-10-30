cd63a618bc9688c1de739b48aa29bedf3f4a4a22
diego-val-vel <diego.val.vel@gmail.com>
2023-10-30 06:56:10 +0000

    topic1 practice is added

---
+++ topic1/practice/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "operations.h"
+
+int main() {
+    double num1 = 10.0, num2 = 5.0;
+    
+    // Utiliza las funciones de operaciones para realizar cálculos y mostrar los resultados.
+    std::cout << "Suma: " << add(num1, num2) << std::endl;
+    std::cout << "Resta: " << subtract(num1, num2) << std::endl;
+    std::cout << "Multiplicación: " << multiply(num1, num2) << std::endl;
+    std::cout << "División: " << divide(num1, num2) << std::endl;
+
+    return 0;
+}
---
+++ topic1/practice/src/operations.cpp
@@ -0,0 +1,20 @@
+#include "operations.h"
+
+double add(double a, double b) {
+    return a + b;
+}
+
+double subtract(double a, double b) {
+    return a - b;
+}
+
+double multiply(double a, double b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    if (b != 0.0) {
+        return a / b;
+    }
+    return 0.0;  // Devuelve 0 si la división es por cero, podría mejorarse con manejo de errores.
+}
