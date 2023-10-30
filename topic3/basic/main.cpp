690e9464df9f95c699d3f37ff098e5d5a7956f18
diego-val-vel <diego.val.vel@gmail.com>
2023-10-30 09:23:48 +0000

    topic3 basic is added

---
+++ topic3/basic/main.cpp
@@ -0,0 +1,7 @@
+// Este es un programa simple en C++ que imprime "Hello, World!" en la consola.
+#include <iostream> // Incluye la biblioteca de entrada y salida de C++
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // Imprime el mensaje en la consola
+    return 0; // Retorna 0 indicando que el programa finalizó exitosamente
+}
