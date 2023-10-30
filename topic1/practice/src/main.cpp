60a35bd8885e3c0ff76f2dc3f9ea5da8c803216f
diego-val-vel <diego.val.vel@gmail.com>
2023-10-30 06:57:49 +0000

    topic1 practice is added

--- topic1/practice/src/main.cpp
+++ topic1/practice/src/main.cpp
@@ -6,7 +6,6 @@ int main() {
     
     // Utiliza las funciones de operaciones para realizar cálculos y mostrar los resultados.
     std::cout << "Suma: " << add(num1, num2) << std::endl;
-    std::cout << "Resta: " << subtract(num1, num2) << std::endl;
     std::cout << "Multiplicación: " << multiply(num1, num2) << std::endl;
     std::cout << "División: " << divide(num1, num2) << std::endl;
 
--- topic1/practice/src/operations.h
+++ topic1/practice/src/operations.h
@@ -3,6 +3,5 @@
 // Funciones de operaciones matemáticas básicas.
 
 double add(double a, double b);
-double subtract(double a, double b);
 double multiply(double a, double b);
 double divide(double a, double b);
