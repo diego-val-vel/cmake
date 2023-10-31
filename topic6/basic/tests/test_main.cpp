ce5ae80b9f4cded4fbb5d15f0fc593b3823a059e
diego-val-vel <diego.val.vel@gmail.com>
2023-10-31 07:29:58 +0000

    Topic6 basic added

---
+++ topic6/basic/tests/test_main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cassert>
+
+int main() {
+    // Realiza una prueba sencilla: verifica si 1 + 1 es igual a 2
+    assert(1 + 1 == 2);
+    std::cout << "Test passed!" << std::endl;
+    return 0;
+}
