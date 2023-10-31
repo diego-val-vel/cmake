e17ec8b0350de01c232b95e1c30761ffe102b551
diego-val-vel <diego.val.vel@gmail.com>
2023-10-31 06:45:38 +0000

    Topic5 practice added

---
+++ topic5/practice/app/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "advanced_library/advanced_math.h"
+
+int main() {
+    double result = advanced_library::power(2.0, 3);
+    std::cout << "Result: " << result << std::endl;  // Debería imprimir 8.0
+    return 0;
+}
