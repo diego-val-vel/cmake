4f11f924b9fcb395a3827ae910aaa42436c94081
diego-val-vel <diego.val.vel@gmail.com>
2023-10-31 07:51:23 +0000

    Topic6 practice added

--- topic6/practice/tests/test_main.cpp
+++ topic6/practice/tests/test_main.cpp
@@ -4,7 +4,5 @@
 
 int main() {
     // Realizar una prueba: verificar si el cuadrado de 3 es 9
-    assert(square(3) == 9);
-    std::cout << "Test passed!" << std::endl;
-    return 0;
+    return 0
 }
