4d5576e03fd85b7a49aea01551b7243096b39f08
diego-val-vel <diego.val.vel@gmail.com>
2023-10-31 07:48:47 +0000

    Topic6 practice added

--- topic6/practice/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/NobleProg/Desktop/cmake/topic6/practice
-# Build directory: C:/Users/NobleProg/Desktop/cmake/topic6/practice/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- topic6/practice/build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/NobleProg/Desktop/cmake/topic6/practice/tests
-# Build directory: C:/Users/NobleProg/Desktop/cmake/topic6/practice/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
